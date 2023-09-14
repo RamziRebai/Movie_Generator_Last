5bb80e2c8b559fce7917d69c2f51add54148f2c8
RamziRebai <ramzi.rebai@esprit.tn>
2023-09-14 15:36:17 +0100

    testing first function

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -9,10 +9,33 @@ const openai= OpenAI(
 exports.handler= async (event) => {
   const {path}= event;
   if (path === '/fetchBotReply') {
-    const result = await performfetchBotReply()
+    const  result = openai.completions.create({
+      model: 'text-ada-001',
+      prompt: `Generate a short enthusiastically message to respond to a user idea"
+      ###
+      user idea:  Let's organize a community cleanup event this weekend!
+      message response: What a fantastic idea! Count me in, and let's make our community shine together this weekend!
+      ###
+      user idea:  How about starting a book club for our friends?
+      message response: I love it! A book club sounds amazing. Let's dive into some great reads and lively discussions together!
+      ###
+      user idea: "${event.body}"
+      message response:
+      `,
+      max_tokens: 30 // defaults to 16
+  })
     return {
       statusCode: 200,
-      body: JSON.stringify(result),
+      body: JSON.stringify({reply: result.data})
+    };
+  }
+  else if(path==='/fetchTitle') {
+    const result= performfetchTitle()
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        reply: result.data
+      })
     };
   } 
   else if(path ==='/fetchBotSynopsis') {
