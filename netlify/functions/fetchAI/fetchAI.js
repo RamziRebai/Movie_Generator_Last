e5391902a9fa908b253345da58df29ab22235424
RamziRebai <ramzi.rebai@esprit.tn>
2023-09-14 18:53:55 +0100

    testing

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,17 +1,34 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+import { OpenAI } from 'openai'
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
 const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+    try {
+      const response = await openai.completions.create({
+        model: 'text-ada-001',
+        prompt: `Generate a short enthusiastically message to respond to a user idea"
+        ###
+        user idea:  Let's organize a community cleanup event this weekend!
+        message response: What a fantastic idea! Count me in, and let's make our community shine together this weekend!
+        ###
+        user idea:  How about starting a book club for our friends?
+        message response: I love it! A book club sounds amazing. Let's dive into some great reads and lively discussions together!
+        ###
+        user idea: "${outline}"
+        message response:
+        `,
+        max_tokens: 30 // defaults to 16
+        })
+        return {
+            statusCode: 200,
+            body: JSON.stringify({reply: response.data})
+        }
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
 }
 
 module.exports = { handler }
+
