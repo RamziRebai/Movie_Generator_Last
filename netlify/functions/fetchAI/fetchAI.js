8da087a122bfcb0fb34ffbed648adbb574ff5f6d
RamziRebai <ramzi.rebai@esprit.tn>
2023-09-14 17:19:38 +0100

    test55

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,15 +1,23 @@
 import { OpenAI } from 'openai';
 
-
 const handler = async (event) => {
-  const openai = OpenAI({
-    apiKey: OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true,
-  });
   try {
-      const response = await openai.completions.create({
-        model: 'text-ada-001',
-        prompt: `Generate a short enthusiastically message to respond to a user idea"
+    // Retrieve the OPENAI_API_KEY from environmental variables
+    const apiKey = process.env.OPENAI_API_KEY;
+
+    // Check if the API key is defined
+    if (!apiKey) {
+      throw new Error('OPENAI_API_KEY is not defined.');
+    }
+
+    const openai = OpenAI({
+      apiKey,
+      dangerouslyAllowBrowser: true,
+    });
+
+    const response = await openai.completions.create({
+      model: 'text-ada-001',
+      prompt: `Generate a short enthusiastic message to respond to a user idea"
         ###
         user idea:  Let's organize a community cleanup event this weekend!
         message response: What a fantastic idea! Count me in, and let's make our community shine together this weekend!
@@ -20,17 +28,25 @@ const handler = async (event) => {
         user idea: "${event.body}"
         message response:
         `,
-        max_tokens: 30 // defaults to 16
-      })
-      return {
-          statusCode: 200,
-          body: JSON.stringify({
-              reply: response                
-          })
-      }
-  } catch (error) {
-      return { statusCode: 500, body: error.toString() }
-  }
-}
+      max_tokens: 30, // defaults to 16
+    });
 
-module.exports = { handler }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        reply: response,
+      }),
+    };
+  } catch (error) {
+    console.error('Error:', error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: 'Internal Server Error',
+      }),
+    };
+  }
+};
+
+module.exports = { handler };
