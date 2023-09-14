8b3c1ed9c35107925210f3c71fe4c844d2ce8532
RamziRebai <ramzi.rebai@esprit.tn>
2023-09-14 15:46:42 +0100

    testing first function

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,106 +1,84 @@
-import {OpenAI} from 'openai'
+import { OpenAI } from 'openai';
 
-const openai= OpenAI(
-  { apiKey: process.env.OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true 
-  }
-)
+const openai = OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
 
-const handler= async (event) => {
-  const {path}= event;
+const handler = async (event) => {
+  const { path } = event;
+  
   if (path === '/fetchAI/fetchBotReply') {
-    const  result = openai.completions.create({
+    const result = openai.completions.create({
       model: 'text-ada-001',
-      prompt: `Generate a short enthusiastically message to respond to a user idea"
+      prompt: `Generate a short enthusiastic message to respond to a user idea"
       ###
-      user idea:  Let's organize a community cleanup event this weekend!
+      user idea: Let's organize a community cleanup event this weekend!
       message response: What a fantastic idea! Count me in, and let's make our community shine together this weekend!
       ###
-      user idea:  How about starting a book club for our friends?
+      user idea: How about starting a book club for our friends?
       message response: I love it! A book club sounds amazing. Let's dive into some great reads and lively discussions together!
       ###
       user idea: "${event.body}"
       message response:
       `,
-      max_tokens: 30 // defaults to 16
-  })
+      max_tokens: 30,
+    });
+    
     return {
       statusCode: 200,
-      body: JSON.stringify({reply: result.data})
+      body: JSON.stringify({ reply: result.data }),
     };
-  }
-  /*
-  else if(path==='/fetchTitle') {
-    const result= performfetchTitle()
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        reply: result.data
-      })
-    };
-  } 
-  else if(path ==='/fetchBotSynopsis') {
-    const result= performfetchTitle()
+  } else if (path === '/fetchTitle') {
+    // Implement performfetchTitle or remove this block if not needed
+    const result = performfetchTitle();
+    
     return {
       statusCode: 200,
       body: JSON.stringify({
-        reply: result.data
-      })
+        reply: result.data,
+      }),
     };
-  }
-  else if(path==='/fetchTitle') {
-    const result= performfetchTitle()
+  } else if (path === '/fetchBotSynopsis') {
+    // Implement performfetchBotSynopsis or remove this block if not needed
+    const result = performfetchBotSynopsis();
+    
     return {
       statusCode: 200,
       body: JSON.stringify({
-        reply: result.data
-      })
+        reply: result.data,
+      }),
     };
-  }
-  else if(path==='/fetchStars') {
-    const result= performfetchStars()
+  } else if (path === '/fetchStars') {
+    // Implement performfetchStars or remove this block if not needed
+    const result = performfetchStars();
+    
     return {
       statusCode: 200,
-      body: JSON.stringify(result)
+      body: JSON.stringify(result),
     };
-  }
-  else if(path==='/fetchImagePrompt') {
-    const result= performfetchImagePrompt()
+  } else if (path === '/fetchImagePrompt') {
+    // Implement performfetchImagePrompt or remove this block if not needed
+    const result = performfetchImagePrompt();
+    
     return {
       statusCode: 200,
-      body: JSON.stringify(result)
+      body: JSON.stringify(result),
     };
-  }
-  else if(path==='/fetchImage') {
-    const result= performfetchImage()
+  } else if (path === '/fetchImage') {
+    // Implement performfetchImage or remove this block if not needed
+    const result = performfetchImage();
+    
     return {
       statusCode: 200,
-      body: JSON.stringify(result)
+      body: JSON.stringify(result),
     };
   }
+
   return {
     statusCode: 400,
-    body: JSON.stringify({message: 'Not Found'})
+    body: JSON.stringify({ message: 'Not Found' }),
   };
 };
 
-
-// function performfetchBotReply(){
-  
-// }
-
-
-// Helper functions for your functionalities
-function performFunctionA() {
-  // Implement functionality A
-  return { message: 'Functionality A executed' };
-}
-
-function performFunctionB() {
-  // Implement functionality B
-  return { message: 'Functionality B executed' };
-};
-*/
-}
-
-module.exports= {handler}
+module.exports = { handler };
